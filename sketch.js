85eaefff332e32e0b2103347b1c596635cf7b723
Ellen Wilson <wilsonladies@MacBook-Pro.local>
2021-04-26 16:08:18 -0400

    added the constrained bodies

--- sketch.js
+++ sketch.js
@@ -5,7 +5,7 @@ const Bodies = Matter.Bodies;
 var engine, world;
 var box1, pig1;
 var backgroundImg,platform;
-
+var Constraintlog
 function preload() {
     backgroundImg = loadImage("sprites/bg.png");
 }
@@ -35,7 +35,8 @@ function setup(){
     log5 = new Log(870,120,150, -PI/7);
 
     bird = new Bird(100,100);
-
+Constraintlog=new Log(180,230,100,PI/2)
+chain= new Chain(bird.body,Constraintlog.body)
 }
 
 function draw(){
@@ -61,4 +62,6 @@ function draw(){
 
     bird.display();
     platform.display();
+    Constraintlog.display()
+    chain.display()
 }
